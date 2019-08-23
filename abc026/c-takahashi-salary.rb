9e9421f8ba9f645c0a28664aa4eab61bf711231b
nixiesquid <audu817@gmail.com>
2019-08-24 07:28:38 +0900

    add c-takahashi-sarary(abc026)

---
+++ abc026/c-takahashi-salary.rb
@@ -0,0 +1,20 @@
+Person = Struct.new(:salary, :subs_salaries, :boss_idx)
+
+n = gets.to_i
+persons = [Person.new(1, [], -1)]
+(1..n - 1).each do |i|
+  persons[i] = Person.new(1, [], gets.to_i - 1)
+end
+
+(n - 1).downto(0) do |i| 
+  if persons[i].subs_salaries.empty?
+    persons[i].salary = 1
+  else
+    persons[i].salary = persons[i].subs_salaries.minmax.inject(:+) + 1
+  end
+  boss_idx = persons[i].boss_idx
+  persons[boss_idx].subs_salaries << persons[i].salary
+  persons[boss_idx].salary = persons[boss_idx].subs_salaries.minmax.inject(:+) + 1
+end
+
+puts persons.first.salary
