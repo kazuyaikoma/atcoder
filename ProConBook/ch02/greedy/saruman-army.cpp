bc9cd29a84eb170d61ead0309d5cefeee0e8ea68
nixiesquid <audu817@gmail.com>
2019-10-04 02:53:01 +0900

    improve saruman-army code

--- ProConBook/ch02/greedy/saruman-army.cpp
+++ ProConBook/ch02/greedy/saruman-army.cpp
@@ -2,22 +2,23 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 6;
+const int N = 10;
 const int R = 10;
-int X[N] = {1, 7, 15, 20, 30, 50};
+int X[N] = {1, 7, 15, 20, 30, 50, 85, 86, 95, 106};
 
 int main() {
   sort(X, X + N);
-  int idx = 0, cnt = 0;
+  int idx = 0, cnt = 0, base = 0;
 
   while (idx < N) {
-    int s = X[idx++];
-    while (idx < N && X[idx] - s <= R) idx++;
-
-    // new point marked black
-    int new_base = X[idx - 1];
-    while (idx < N && X[idx] - new_base <= R) idx++;
+    while (X[base + 1] - X[idx] <= R) base++;
+    idx = base;
+    while (X[idx + 1] - X[base] <= R) idx++;
     cnt++;
+    idx++;
+    // make base equal to idx for next loop
+    base = idx;
+    if (idx >= N) break;
   }
 
   printf("%d\n", cnt);
