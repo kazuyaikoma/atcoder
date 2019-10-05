cb50d4f612ca535cafb017575126d75dec3799f2
nixiesquid <audu817@gmail.com>
2019-10-06 05:46:28 +0900

    fix knapsack double looped

--- ProConBook/ch02/dp/knapsack-double-looped.cpp
+++ ProConBook/ch02/dp/knapsack-double-looped.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 const int N = 4;
-int limit_weight = 5;
+const int W = 5;
 typedef pair<int, int> Item;
 // 前者が重さ、後者が価値
 vector<Item> items = {{2, 3}, {1, 2}, {3, 4}, {2, 2}};
 // メモ
-int dp[N][N];
+int dp[N+1][W+1];
 // interface
-int calc(int idx, int limit_weight);
+int calc(int idx, int W);
 
 int main() {
   for (int i = N - 1; i >= 0; i--) {
-    for (int w = 0; w <= limit_weight; w++) {
+    for (int w = 0; w <= W; w++) {
       if (items[i].first > w) {
         dp[i][w] = dp[i+1][w];
       } else {
@@ -27,6 +27,6 @@ int main() {
     }
   }
 
-  printf("%d\n", dp[0][limit_weight]);
+  printf("%d\n", dp[0][W]);
   return 0;
 }
