5ac56cc11d9d6b0eac5956c5cce9415d71ff4f39
nixiesquid <audu817@gmail.com>
2019-11-06 19:54:33 +0900

    fix frog program

--- AtCoder/dp-contest/a-frog-1/src/main.rs
+++ AtCoder/dp-contest/a-frog-1/src/main.rs
@@ -12,18 +12,17 @@ where T: FromStr, <T as FromStr>::Err : Debug {
 fn main() {
     let _vec: Vec<usize> = read_line();
     let hs: Vec<i32> = read_line();
-
     let mut dp: Vec<i32> = Vec::new();
-    // init with zero
+
     for _ in 0..hs.len() { dp.push(0); }
-    dp[0] = 0;
-    dp[1] = (hs[0] - hs[1]).abs();
+    dp[1] = (hs[1] - hs[0]).abs();
 
     for i in 2..hs.len() {
         dp[i] = cmp::min(
-                    dp[i - 2] + (hs[i] - hs[i - 2]).abs(),
-                    dp[i - 1] + (hs[i] - hs[i - 1]).abs(),
-                );
+            dp[i - 1] + (hs[i] - hs[i - 1]).abs(),
+            dp[i - 2] + (hs[i] - hs[i - 2]).abs(),
+        );
     }
+
     println!("{}", dp.last().unwrap());
 }
--- AtCoder/dp-contest/b-frog-2/src/main.rs
+++ AtCoder/dp-contest/b-frog-2/src/main.rs
@@ -12,19 +12,18 @@ fn main() {
     let vec: Vec<usize> = read_line();
     let k = vec[1];
     let hs: Vec<i32> = read_line();
-
     let mut dp: Vec<i32> = Vec::new();
-    // init with zero
-    for _ in 0..hs.len() { dp.push(0); }
-    dp[0] = 0;
-    dp[1] = (hs[0] - hs[1]).abs();
 
-    // calculate each dp
+    for _ in 0..hs.len() { dp.push(0); }
+    dp[1] = (hs[1] - hs[0]).abs();
+
     for i in 2..hs.len() {
-        let start_idx: usize = if (i as i32) - (k as i32) >= 0 { i - k } else { 0 };
+        let start: usize = if (i as i32) - (k as i32) >= 0 { i - k } else { 0 };
         let mut costs: Vec<i32> = Vec::new();
-        for j in start_idx..i { costs.push(dp[j] + (hs[i] - hs[j]).abs()); }
-        dp[i] = *(costs.iter().min().unwrap());
+
+        for j in start..i { costs.push(dp[j] + (hs[i] - hs[j]).abs()); }
+        dp[i] = costs.into_iter().min().unwrap();
     }
+
     println!("{}", dp.last().unwrap());
 }
