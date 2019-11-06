a92f0c90f4382b01cb0843601ad1f3294cfee714
nixiesquid <audu817@gmail.com>
2019-11-06 19:54:49 +0900

    delete unnecessary programs

--- RustPractice/map-gets-fun-pointer/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let v = vec!["I", "am", "NixieSquid"]
-        .into_iter()
-        // .map(|s| s.len()) を関数ポインタで書き換えた
-        .map(str::len)
-        .collect::<Vec<_>>();
-    assert_eq!(v, vec![1, 2, 10]);
-}
