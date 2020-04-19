39966e0d4a1a2fe60ee15457267f6e70447132fc
nixiesquid <audu817@gmail.com>
2020-04-20 08:42:11 +0900

    add abc163

---
+++ AtCoder/abc163/abc163/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define PI 3.141592653589
+using namespace std;
+
+
+void solve(long long N){
+  cout << std::fixed << std::setprecision(20) << 2 * N * PI << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    solve(N);
+    return 0;
+}
