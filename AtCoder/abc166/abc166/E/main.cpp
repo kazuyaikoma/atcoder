8a6ee2ba86889268110b26ba9c09bc63b24f794b
nixiesquid <audu817@gmail.com>
2020-05-04 16:43:26 +0900

    add abc166 E

--- AtCoder/abc166/abc166/E/main.cpp
+++ AtCoder/abc166/abc166/E/main.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 void solve(long long N, std::vector<long long> H){
   long long cnt = 0;
-  for (long long i=N-1; i>=0; i--) {
-    long long partner_max = i + H[i];
-    if (partner_max < 0) continue;
-    for (long long j=0; j<partner_max; j++) if (i-j == H[i]+H[j]) cnt++;
+  unordered_map<long, long> m1, m2;
+  // I < J, I = i + H[i], J = j - H[j]を満たすi, jを探せば良い
+  for (long long i=0; i<N; i++) {
+    // m1, m2の2つのmapを使い、
+    // 例えばJにおいてインクリメントしたマップの値にIでアクセスした結果0より大きければ、
+    // それがcntに加算される
+    cnt += m1[i+H[i]];
+    cnt += m2[i-H[i]];
+    // それぞれ、値へのアクセス時とは逆のインデックスによりインクリメント
+    // これにより、二者のペアがマッチした状態のmapの値に、上の二行でアクセスできる
+    m2[i+H[i]]++;
+    m1[i-H[i]]++;
   }
   cout << cnt << endl;
 }
