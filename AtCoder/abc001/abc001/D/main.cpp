196098a896147c64f976e3c8cbe35b41704abd63
nixiesquid <audu817@gmail.com>
2020-05-05 18:10:31 +0900

    fix abc001 D

--- AtCoder/abc001/abc001/D/main.cpp
+++ AtCoder/abc001/abc001/D/main.cpp
@@ -1,74 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> split(string str, char del) {
-  int first = 0;
-  int last = str.find_first_of(del);
-  vector<string> result;
 
-  while (first < str.size()) {
-    result.push_back(str.substr(first, last - first));
-    first = last + 1;
-    last = str.find_first_of(del, first);
-    if (last == string::npos) last = str.size();
-  }
-  return result;
-}
-
-int fixed(int time, bool first) {
-  int one_digit = time % 10;
-  int two_digit = time % 100;
-  if (!first && 57<=two_digit) return time + (100-two_digit);
-
-  if (first) {
-    if (one_digit < 5) return time - one_digit;
-    else return time - one_digit + 5;
-  } else {
-    if (0 < one_digit && one_digit < 5) return time + (5 - one_digit);
-    else if (5 < one_digit && one_digit < 10) return time + (10 - one_digit);
-  }
-  return time;
-}
-
-void solve(long long N, std::vector<string> A){
-  map<int, int> m;
-  for (string a : A) {
-    vector<string> time = {};
-    time = split(a, '-');
-    int begin = fixed(stoi(time[0]), true), end = fixed(stoi(time[1]), false);
-    m[begin] = end;
-  }
-
-  vector<string> ans = {};
-  int prev_begin = -1, prev_end = -1;
-  for (auto elm : m) {
-    if (prev_end < elm.first) {
-      if (prev_begin != -1) {
-        cout << setw(4) << setfill('0') << prev_begin;
-        cout << '-';
-        cout << setw(4) << setfill('0') << prev_end << endl;
-      }
-
-      prev_begin = elm.first;
-      prev_end = elm.second;
-      continue;
-    }
-    prev_end = max(prev_end, elm.second);
-  }
-  cout << setw(4) << setfill('0') << prev_begin;
-  cout << '-';
-  cout << setw(4) << setfill('0') << prev_end << endl;
+string fixed(int t) {
+  // それぞれ時間・分に直す
+  int h = t/12;
+  int m = (t%12)*5;
+  return ( h<10 ? "0" : "" ) + to_string(h) + (m<10 ? "0" : "" ) + to_string(m);
 }
 
 int main(){
   long long N;
   scanf("%lld",&N);
   vector<string> A(N);
+  // imos法のための配列
+  int rain[12*24+1] = {};
   for(int i = 0 ; i < N ; i++){
     string str;
     cin >> str;
-    A[i] = str;
+
+    int sh, sm, eh, em;
+    sh = stoi(str.substr(0, 2));
+    sm = stoi(str.substr(2, 2));
+    eh = stoi(str.substr(5, 2));
+    em = stoi(str.substr(7, 2));
+
+    rain[sh*12+sm/5]++;
+    // imos法では、区間の1つ先をデクリメントするため(5-1)をemに足している
+    rain[eh*12+(em+(5-1))/5]--;
   }
-  solve(N, std::move(A));
+
+  for (int i=1; i<12*24+1; i++) rain[i] += rain[i-1];
+
+  for (int i=0; i<12*24+1; i++) {
+    if (i == 0) {
+      if(rain[i] > 0) cout << fixed(i) << "-";
+    } else if (i == 288) {
+      if (rain[i-1] > 0) cout << fixed(i) << endl;
+    } else {
+      if (rain[i] > 0) {
+       if (rain[i-1] == 0) cout << fixed(i) << "-";
+      } else if (rain[i-1] > 0) cout << fixed(i) << endl;
+    }
+  }
   return 0;
 }
