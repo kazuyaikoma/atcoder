76dc5078edea783495ef0d2b8b71b12f095ce452
nixiesquid <audu817@gmail.com>
2020-06-06 07:51:02 +0900

    add nomura C

--- AtCoder/nomura2020/nomura2020/C/main.cpp
+++ AtCoder/nomura2020/nomura2020/C/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 void solve(long long N, std::vector<long long> A){
   int ret = 0;
-  vector<int> slots(N, 0);
+  vector<int> slots = {1};
 
   int slot = 1;
   for (int i=1; i<=N; i++) {
-    slot = slot * 2 - A[i];
+    slot = slot * 2 - A[i+1];
     if (slot < 0) {
       ret = -1;
       break;
@@ -16,32 +16,14 @@ void solve(long long N, std::vector<long long> A){
     slots.push_back(slot);
   }
 
-
-  for (int i=0; i<N; i++) slots[i] = pow(2, i+1);
-  for (int i=0; i<N; i++) {
-    if (A[i+1] > slots[i]) ret = -1;
-  }
-
   if (ret < 0) {
     cout << -1 << endl;
     return;
   }
 
-  int idx = 1;
-  for (int i=N; i>=1; i--) {
-    if (A[i] < 1) continue;
-
-    bool invalid = true;
-    for (int j=idx; j<=i; j++) {
-      slots[j] -= A[i];
-      if (slots[j] < 0) idx++;
-      else invalid = false;
-    }
-    if (invalid) {
-      ret = -1;
-      break;
-    }
-    ret += A[i] * i;
+  for (int i=0; i<N; i++) slots[i] = pow(2, i+1);
+  for (int i=0; i<N; i++) {
+    if (A[i+1] > slots[i]) ret = -1;
   }
 
   cout << ret << endl;
