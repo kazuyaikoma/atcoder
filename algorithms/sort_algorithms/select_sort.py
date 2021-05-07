f608d73b37698dcb56fbde6a55ed4839e8497d28
nixiesquid <audu817@gmail.com>
2021-05-07 17:48:39 +0900

    improve select_sort.py

--- algorithms/sort_algorithms/select_sort.py
+++ algorithms/sort_algorithms/select_sort.py
@@ -3,16 +3,21 @@ from typing import List
 
 
 class TestSelectSort(unittest.TestCase):
-    # 選択ソート
+    """
+    選択ソート
+    ループごとに最小のものを選ぶ→先頭と随時交換していく
+
+    時間計算量 O(N^2)
+    空間計算量 O(1)
+    """
     def sort(self, arr: List[int]) -> List[int]:
-        for i in range(0, len(arr)-1):
-            # そのループで最小値をとるindex
+        for i, elm in enumerate(arr):
             min_idx = i
             for j in range(i, len(arr)):
                 if arr[j] < arr[min_idx]:
                     min_idx = j
-
-            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            # 空間計算量 O(1)とするため、新たにリストを作らずswap
+            arr[i], arr[min_idx] = arr[min_idx], elm
 
         return arr
 
