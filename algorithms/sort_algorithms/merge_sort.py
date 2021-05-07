576fc2ee49522c88ad14d28e7c3adac12487c2aa
nixiesquid <audu817@gmail.com>
2021-05-07 23:19:57 +0900

    add merge_sort.py

---
+++ algorithms/sort_algorithms/merge_sort.py
@@ -0,0 +1,52 @@
+import unittest
+from typing import List
+
+
+class TestMergeSort(unittest.TestCase):
+    """
+    マージソート
+
+    時間計算量 O(n log n)
+    空間計算量 O(n)
+    安定ソート
+    """
+    def sort(self, arr: List[int]) -> List[int]:
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+
+        left_arr = self.sort(left_arr)
+        right_arr = self.sort(right_arr)
+
+        return self.merge(left_arr, right_arr)
+
+    def merge(self, left_arr: List[int], right_arr: List[int]) -> List[int]:
+        merged = []
+        left, right = 0, 0
+
+        while left < len(left_arr) and right < len(right_arr):
+            if left_arr[left] < right_arr[right]:
+                merged.append(left_arr[left])
+                left += 1
+            else:
+                merged.append(right_arr[right])
+                right += 1
+
+        if left < len(left_arr):
+            merged.extend(left_arr[left:])
+        elif right < len(right_arr):
+            merged.extend(right_arr[right:])
+
+        return merged
+
+    def test_sort(self):
+        arr = [4, 3, 2, 5, 1]
+        arr = self.sort(arr)
+        self.assertEqual(arr, [1, 2, 3, 4, 5])
+
+
+if __name__ == '__main__':
+    unittest.main()
