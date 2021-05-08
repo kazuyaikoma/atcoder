811b2c0bde3333447e673ad730aaaf8ed3bbd0d0
nixiesquid <audu817@gmail.com>
2021-05-09 02:45:14 +0900

    fix C solution(ABC200)

--- AtCoder/abc200/C/main.py
+++ AtCoder/abc200/C/main.py
@@ -4,17 +4,22 @@ from typing import List
 
 
 def solve(N: int, A: List[int]):
-    # A.sort()
-    # # 累積和の配列
-    # cums = []
-    # for i, num in enumerate(A):
-    #     if i == len(A)-1:
-    #         continue
+    for i, num in enumerate(A):
+        A[i] = num % 200
 
-    #     cums.append(A[i+1] - A[i])
+    A.sort()
+    cums = [0] * 200
+    for a in A:
+        cums[a] += 1
 
-    # cums.reverse()
-    pass
+    sum = 0
+    for cum in cums:
+        if cum == 0 or cum == 1:
+            continue
+        else:
+            sum += cum * (cum-1) / 2
+
+    print(int(sum))
 
 
 def main():
