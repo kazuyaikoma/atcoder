cd677f81c936242fdef8561b0909bbeb173de26a
nixiesquid <audu817@gmail.com>
2021-05-08 10:27:57 +0900

    add comment to insert_bin_sort.py

--- algorithms/sort_algorithms/insert_bin_sort.py
+++ algorithms/sort_algorithms/insert_bin_sort.py
@@ -12,12 +12,18 @@ class TestInsertSort(unittest.TestCase):
     空間計算量 O(1)
     安定ソート
     """
+
+    # 挿入ソートにおける挿入位置を二分探索し、挿入箇所のidx+1を返す
     def search(self, arr: List[int], low: int, high: int, target: int) -> int:
         if low == high:
             if target < arr[low]:
                 return low
             else:
-                return low + 1
+                # arr[high] <= target; ということなので、targetのidxを返すことになる
+                # return low + 1; と等価だが、highの方がイメージに合っている
+                # これが返される場合、配列のソート順は変わらない(targetのidxを返すので)
+                return high + 1
+        # lowがhighを越したのであれば、その時点でlowを返す
         elif high < low:
             return low
 
