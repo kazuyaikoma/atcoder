75116bce33e6421e137d3781fcc687e239dfe16a
nixiesquid <audu817@gmail.com>
2021-05-08 23:08:20 +0900

    improve solve

--- AtCoder/abc165/A/main.py
+++ AtCoder/abc165/A/main.py
@@ -2,13 +2,23 @@
 import sys
 
 
-def solve(K: int, A: int, B: int):
+# 全探索の場合
+def brute_force_solve(K: int, A: int, B: int):
     for i in range(A, B+1):
         if i % K == 0:
             print('OK')
             return
 
     print('NG')
+
+
+def solve(K: int, A: int, B: int):
+    x = A // K
+    y = B // K
+    if x < y or A % K == 0:
+        print('OK')
+    else:
+        print('NG')
 
 
 def main():
