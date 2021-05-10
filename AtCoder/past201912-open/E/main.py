63069de1ec161f6ce79ea7cf88d33f5006438853
nixiesquid <audu817@gmail.com>
2021-05-10 17:55:16 +0900

    fix bug at E

--- AtCoder/past201912-open/E/main.py
+++ AtCoder/past201912-open/E/main.py
@@ -6,7 +6,7 @@ def print_graph(N: int, graph: List[int]):
     for i in range(N):
         ans = ""
         for j in range(N):
-            if j in graph[i]:
+            if graph[i][j]:
                 ans += 'Y'
             else:
                 ans += 'N'
@@ -19,50 +19,37 @@ def solve(N: int, Q: int, queries: List[int], graph: List[int]):
         a = query[1] - 1
         if mark == 1:
             b = query[2] - 1
-            graph[a].append(b)
+            graph[a][b] = True
         elif mark == 2:
-            followers = get_followers(graph, a)
-            graph[a] += followers
+            for i in range(N):
+                if graph[i][a]:
+                    graph[a][i] = True
         elif mark == 3:
-            follow_follows = get_follow_follows(graph, a)
-            graph[a] += follow_follows
+            to_follow = []
+
+            for i in range(N):
+                if graph[a][i]:
+                    for j in range(N):
+                        if graph[i][j] and j != a:
+                            to_follow.append(j)
+            for f in to_follow:
+                graph[a][f] = True
 
     print_graph(N, graph)
 
 
-# aをフォローするユーザー群を取得
-def get_followers(graph: List[int], a: int) -> List[int]:
-    followers = []
-    for i, follows in enumerate(graph):
-        if i == a:
-            continue
-        if a in follows:
-            followers.append(i)
-
-    return list(set(followers))
-
-
-# aがフォローするユーザー群(x)のさらにフォローしてるユーザー群を取得
-def get_follow_follows(graph: List[int], a: int) -> List[int]:
-    users = []
-    for x in graph[a]:
-        users += graph[x]
-    if a in users:
-        users.remove(a)
-
-    return list(set(users))
-
-
 def main():
     N, Q = map(int, input().split())
+    graph = []
+    for _ in range(N):
+        row = [False for _ in range(N)]
+        graph.append(row)
 
     queries = []
     for _ in range(Q):
         query = list(map(int, input().split()))
         queries.append(query)
 
-    # aがbをフォローしていることを示す隣接リスト
-    graph = [[] for _ in range(N)]
     solve(N, Q, queries, graph)
 
 
