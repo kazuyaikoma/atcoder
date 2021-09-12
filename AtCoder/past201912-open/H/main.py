caee8ac7d52ab8378c7b967cb71b03db094adf76
nixiesquid <audu817@gmail.com>
2021-09-12 13:25:18 +0900

    refactor H

--- AtCoder/past201912-open/H/main.py
+++ AtCoder/past201912-open/H/main.py
@@ -1,59 +1,66 @@
 #!/usr/bin/env python3
 from typing import List
+import math
 
 
-def solve(cards: List[int], Q: int, S: List[List[int]]):
+def solve(cards: List[int], Q: int, queries: List[List[int]]):
     sell = 0
 
-    # 全種類販売で売った1種類あたりの枚数
-    all_cnt = 0
     # セット販売で売った1種類あたりの枚数
-    set_cnt = 0
-    # セット販売対象のcardsの最小値を記録
-    min_cards_set = 1000000001
-    # セット販売対象ではない(全ての)cardsの最小値を記録
-    min_cards_all = min(cards)
+    set_sell = 0
+    # 全種類販売で売った1種類あたりの枚数
+    all_sell = 0
 
+    # セット販売対象のcardsの最小値を記録
+    min_cards_set = max(cards)
+    # セット販売対象以外のcardsの最小値を記録
+    min_cards_others = max(cards)
+
+    # セット販売の最小値を算出
     for i in range(len(cards)):
-        if i % 2 == 0:
+        if is_set_card(i):
             min_cards_set = min(min_cards_set, cards[i])
 
-    for query in S:
+    for query in queries:
         if query[0] == 1:
             x = query[1] - 1
             a = query[2]
 
-            # 値を考慮する場合はset_cnt(セット販売)も考慮に入れる
+            # 値を考慮する場合はset_sell(セット販売)も考慮に入れる
             delta = 0
-            if x % 2 == 0:
-                delta = set_cnt
-            if a <= cards[x] - all_cnt - delta:
+            if is_set_card(x):
+                delta = set_sell
+
+            if a <= cards[x] - all_sell - delta:
                 cards[x] -= a
                 sell += a
-                if x % 2 == 0:
+
+                if is_set_card(x):
                     min_cards_set = min(min_cards_set, cards[x])
-                min_cards_all = min(min_cards_all, cards[x])
+                min_cards_others = min(min_cards_others, cards[x])
         elif query[0] == 2:
             a = query[1]
-            if a <= min_cards_set - all_cnt - set_cnt:
-                set_cnt += a
+            if a <= min_cards_set - all_sell - set_sell:
+                set_sell += a
         elif query[0] == 3:
             a = query[1]
             if a <= min(
-                    min_cards_set - all_cnt - set_cnt,
-                    min_cards_all - all_cnt
+                    min_cards_set - all_sell - set_sell,
+                    min_cards_others - all_sell
             ):
-                all_cnt += a
+                all_sell += a
 
     # セット販売した枚数を合算
-    for i in range(len(cards)):
-        if i % 2 == 0:
-            sell += set_cnt
+    sell += set_sell * math.ceil(len(cards)/2)
 
     # 全種類販売した枚数を合算
-    sell += len(cards) * all_cnt
+    sell += all_sell * len(cards)
 
     print(sell)
+
+
+def is_set_card(x: int) -> bool:
+    return x % 2 == 0
 
 
 def main():
@@ -61,11 +68,11 @@ def main():
     cards = list(map(int, input().split()))
     Q = int(input())
 
-    S = []
+    queries = []
     for _ in range(Q):
         query = list(map(int, input().split()))
-        S.append(query)
-    solve(cards, Q, S)
+        queries.append(query)
+    solve(cards, Q, queries)
 
 
 if __name__ == '__main__':
