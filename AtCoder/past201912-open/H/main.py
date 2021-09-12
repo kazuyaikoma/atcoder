22af134865ef532b6996fd26ba946bd97606bca0
nixiesquid <audu817@gmail.com>
2021-09-12 12:29:09 +0900

    refactor past201912 H

--- AtCoder/past201912-open/H/main.py
+++ AtCoder/past201912-open/H/main.py
@@ -2,20 +2,21 @@
 from typing import List
 
 
-def solve(N: int, C: List[int], Q: int, S: List[List[int]]):
+def solve(cards: List[int], Q: int, S: List[List[int]]):
     sell = 0
-    # 全種類販売で売った1種類あたりの枚数
-    set_cnt = 0
-    # セット販売で売った1種類あたりの枚数
-    all_cnt = 0
-    # セット販売対象のCの最小値を記録
-    min_C_set = 1000000001
-    # セット販売対象ではない(全ての)Cの最小値を記録
-    min_C_all = min(C)
 
-    for i in range(N):
+    # 全種類販売で売った1種類あたりの枚数
+    all_cnt = 0
+    # セット販売で売った1種類あたりの枚数
+    set_cnt = 0
+    # セット販売対象のcardsの最小値を記録
+    min_cards_set = 1000000001
+    # セット販売対象ではない(全ての)cardsの最小値を記録
+    min_cards_all = min(cards)
+
+    for i in range(len(cards)):
         if i % 2 == 0:
-            min_C_set = min(min_C_set, C[i])
+            min_cards_set = min(min_cards_set, cards[i])
 
     for query in S:
         if query[0] == 1:
@@ -26,42 +27,45 @@ def solve(N: int, C: List[int], Q: int, S: List[List[int]]):
             delta = 0
             if x % 2 == 0:
                 delta = set_cnt
-            if a <= C[x] - all_cnt - delta:
-                C[x] -= a
+            if a <= cards[x] - all_cnt - delta:
+                cards[x] -= a
                 sell += a
                 if x % 2 == 0:
-                    min_C_set = min(min_C_set, C[x])
-                min_C_all = min(min_C_all, C[x])
+                    min_cards_set = min(min_cards_set, cards[x])
+                min_cards_all = min(min_cards_all, cards[x])
         elif query[0] == 2:
             a = query[1]
-            if a <= min_C_set - all_cnt - set_cnt:
+            if a <= min_cards_set - all_cnt - set_cnt:
                 set_cnt += a
         elif query[0] == 3:
             a = query[1]
-            if a <= min(min_C_set - all_cnt - set_cnt, min_C_all - all_cnt):
+            if a <= min(
+                    min_cards_set - all_cnt - set_cnt,
+                    min_cards_all - all_cnt
+            ):
                 all_cnt += a
 
     # セット販売した枚数を合算
-    for i in range(N):
+    for i in range(len(cards)):
         if i % 2 == 0:
             sell += set_cnt
 
     # 全種類販売した枚数を合算
-    sell += N * all_cnt
+    sell += len(cards) * all_cnt
 
     print(sell)
 
 
 def main():
-    N = int(input())
-    C = list(map(int, input().split()))
+    _ = int(input())
+    cards = list(map(int, input().split()))
     Q = int(input())
 
     S = []
     for _ in range(Q):
         query = list(map(int, input().split()))
         S.append(query)
-    solve(N, C, Q, S)
+    solve(cards, Q, S)
 
 
 if __name__ == '__main__':
