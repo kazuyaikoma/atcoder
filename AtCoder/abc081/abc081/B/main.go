de49bb55dc2207055aed47ac5188101bf1aef49e
nixii <nixii0986@gmail.com>
2022-07-07 10:38:33 +0900

    :+1: add abc081

--- AtCoder/abc081/abc081/B/main.go
+++ AtCoder/abc081/abc081/B/main.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-const MOD = 2
+func reverse(s string) string {
+	runes := []rune(s)
+	for i := 0; i < len(runes)/2; i++ {
+		runes[i], runes[len(runes)-i-1] = runes[len(runes)-i-1], runes[i]
+	}
+	return string(runes)
+}
+
+func cntZero(bin string) int {
+	var cnt int
+	for _, s := range bin {
+		if s == '0' {
+			cnt++
+		} else {
+			return cnt
+		}
+	}
+	return cnt
+}
+
+func count(bins []string) int {
+	cnt := cntZero(bins[0])
+	for _, bin := range bins {
+		newCnt := cntZero(bin)
+		if newCnt < cnt {
+			cnt = newCnt
+		}
+	}
+	return cnt
+}
 
 func solve(N int64, A []int64) {
-
+	var bins []string
+	for _, a := range A {
+		bins = append(bins, reverse(fmt.Sprintf("%b", a)))
+	}
+	fmt.Println(count(bins))
 }
 
 func main() {
@@ -19,12 +53,12 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    A := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(N, A)
 }
