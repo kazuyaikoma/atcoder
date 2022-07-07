fa4276cc867722d353fd484fbc3429d8cd42ab89
nixii <nixii0986@gmail.com>
2022-07-08 03:11:43 +0900

    :+1: add abc088 B

--- AtCoder/abc088/abc088/B/main.go
+++ AtCoder/abc088/abc088/B/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func solve(N int64, a []int64) {
-
+	// 要件上安定ソートは使わなくてOK
+	sort.Slice(a, func(i, j int) bool { return a[i] > a[j] })
+	var alice, bob int64
+	for i, n := range a {
+		if i%2 == 0 {
+			alice += n
+		} else {
+			bob += n
+		}
+	}
+	fmt.Println(alice - bob)
 }
 
 func main() {
@@ -17,12 +29,12 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    a := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        a[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	a := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		a[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
 	solve(N, a)
 }
