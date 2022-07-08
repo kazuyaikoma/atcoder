f9ffafccd5a34b3c8bb282a4ddd80631efddb21c
nixii <nixii0986@gmail.com>
2022-07-08 23:14:16 +0900

    :+1: add abc085 C

--- AtCoder/abc085/abc085/C/main.go
+++ AtCoder/abc085/abc085/C/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-func solve(N int64, Y int64) {
+const (
+	a = 10000
+	b = 5000
+	c = 1000
+)
 
+func calc(N, Y int) string {
+	for i := 0; i <= N; i++ {
+		for j := 0; j <= N-i; j++ {
+			k := N - i - j
+			if a*i+b*j+c*k == Y {
+				return fmt.Sprintf("%d %d %d", i, j, k)
+			}
+		}
+	}
+	return fmt.Sprintf("%d %d %d", -1, -1, -1)
+}
+
+func solve(N int64, Y int64) {
+	s := calc(int(N), int(Y))
+	fmt.Println(s)
 }
 
 func main() {
@@ -17,10 +37,10 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var Y int64
-    scanner.Scan()
-    Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var Y int64
+	scanner.Scan()
+	Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 	solve(N, Y)
 }
