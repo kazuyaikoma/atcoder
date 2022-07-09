746fee77f250033a257528ca44fd74e04dfffe18
nixii <nixii0986@gmail.com>
2022-07-09 12:59:11 +0900

    :+1: solved abc088 C

--- AtCoder/abc088/abc088/C/main.go
+++ AtCoder/abc088/abc088/C/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,7 +11,28 @@ const YES = "Yes"
 const NO = "No"
 
 func solve(c [][]int64) {
+	if len(c) < 1 {
+		fmt.Println(YES)
+		return
+	}
+	// 先に最小の組み合わせを特定
+	base := c[0]
+	for _, arr := range c {
+		if arr[0] < base[0] {
+			base = arr
+		}
+	}
 
+	for _, arr := range c {
+		diff := arr[0] - base[0]
+		for i := 0; i < 3; i++ {
+			if base[i] != arr[i]-diff {
+				fmt.Println(NO)
+				return
+			}
+		}
+	}
+	fmt.Println(YES)
 }
 
 func main() {
@@ -20,14 +42,14 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	c := make([][]int64, 3)
-    for i := int64(0); i < 3; i++ {
-    	c[i] = make([]int64, 3)
-    }
-    for i := int64(0); i < 3; i++ {
-        for j := int64(0); j < 3; j++ {
-            scanner.Scan()
-            c[i][j], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-        }
-    }
+	for i := int64(0); i < 3; i++ {
+		c[i] = make([]int64, 3)
+	}
+	for i := int64(0); i < 3; i++ {
+		for j := int64(0); j < 3; j++ {
+			scanner.Scan()
+			c[i][j], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		}
+	}
 	solve(c)
 }
