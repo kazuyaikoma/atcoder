e79448959c070e33f70d3b9233bda5c3f4fda05e
nixii <nixii0986@gmail.com>
2022-07-16 00:24:35 +0900

    :+1: solved past202004-open D

--- AtCoder/past202004-open/past202004-open/D/main.go
+++ AtCoder/past202004-open/past202004-open/D/main.go
@@ -2,12 +2,56 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strconv"
 )
 
-func solve(S string) {
+// 同じ長さの文字列の比較
+func check(s, t string) bool {
+	for i := 0; i < len(s); i++ {
+		if t[i] == '.' {
+			continue
+		}
+		if s[i] != t[i] {
+			return false
+		}
+	}
 
+	return true
+
+}
+
+func isMatch(s, t string) bool {
+	for i := 0; i <= len(s)-len(t); i++ {
+		if check(s[i:i+len(t)], t) {
+			return true
+		}
+	}
+	return false
+}
+
+func solve(S string) {
+	str := "abcdefghijklmnopqrstuvwxyz."
+	var cnt int
+	for _, s1 := range str {
+		t := string(s1)
+		if isMatch(S, t) {
+			cnt++
+		}
+		for _, s2 := range str {
+			t = string(s1) + string(s2)
+			if isMatch(S, t) {
+				cnt++
+			}
+			for _, s3 := range str {
+				t = string(s1) + string(s2) + string(s3)
+				if isMatch(S, t) {
+					cnt++
+				}
+			}
+		}
+	}
+	fmt.Println(cnt)
 }
 
 func main() {
@@ -17,7 +61,7 @@ func main() {
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
 	var S string
-    scanner.Scan()
-    S = scanner.Text()
+	scanner.Scan()
+	S = scanner.Text()
 	solve(S)
 }
