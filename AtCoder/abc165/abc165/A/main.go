1462dc27cca953cc16f62b91789849ce7b3ccf8d
nixii <nixii0986@gmail.com>
2022-07-16 00:24:08 +0900

    :+1: remove tool_versions, refine abc165 a

--- AtCoder/abc165/abc165/A/main.go
+++ AtCoder/abc165/abc165/A/main.go
@@ -8,10 +8,8 @@ import (
 )
 
 func solve(K int64, A int64, B int64) {
-	// A以下のKの最大倍数を求める
-	// その数字、もしくはそれに+Kした数字がAからBに収まっているかを調べる
-	n := A / K
-	if (A <= n*K && n*K <= B) || (A <= (n+1)*K && (n+1)*K <= B) {
+	// NOTES: https://scrapbox.io/rustacean/ABC165_Aの数学的解法
+	if A/K < B/K || A == A/K*K {
 		fmt.Println("OK")
 	} else {
 		fmt.Println("NG")
