9978cd677e54f03ec753ba590eff3c1fe2ad2301
nixii <nixii0986@gmail.com>
2022-07-21 01:10:09 +0900

    :+1: solved past202005-open G

--- AtCoder/past202005-open/past202005-open/G/main.go
+++ AtCoder/past202005-open/past202005-open/G/main.go
@@ -2,12 +2,58 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
-func solve(N int64, X int64, Y int64, x []int64, y []int64) {
+func solve(N, X, Y int, xs, ys []int) {
+	const margin = 201
+	// 壁となる2次元配列 (403 x 403)
+	// 問題制約として"無限に広がる二次元グリッド"とあるので、201 x 201の二次元空間をはみ出して別のマスに移動が可能。そのため403 x 403。
+	// true: 通路, false: 壁
+	var walls [][]bool
+	for i := 0; i < margin*2+1; i++ {
+		var blk []bool
+		for j := 0; j < margin*2+1; j++ {
+			blk = append(blk, true)
+		}
+		walls = append(walls, blk)
+	}
+	// 問題から与えられた壁の反映
+	walls[0+margin][0+margin] = false
+	for i := 0; i < N; i++ {
+		walls[xs[i]+margin][ys[i]+margin] = false
+	}
 
+	// index 0: x
+	// index 1: y
+	// index 2: cnt
+	q := [][]int{{0, 0, 0}}
+	deltas := [][]int{
+		{1, 1},
+		{0, 1},
+		{-1, 1},
+		{1, 0},
+		{-1, 0},
+		{0, -1},
+	}
+	for len(q) > 0 {
+		x, y, cnt := q[0][0], q[0][1], q[0][2]
+		q = q[1:]
+		if x == X && y == Y {
+			fmt.Println(cnt)
+			return
+		}
+		for _, delta := range deltas {
+			nx, ny, nCnt := x+delta[0], y+delta[1], cnt+1
+			if -201 <= nx && nx <= 201 && -201 <= ny && ny <= 201 && walls[nx+margin][ny+margin] {
+				q = append(q, []int{nx, ny, nCnt})
+				walls[nx+margin][ny+margin] = false
+			}
+		}
+	}
+	fmt.Println(-1)
 }
 
 func main() {
@@ -16,22 +62,22 @@ func main() {
 	const maxBufSize = 1000000
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
-	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var X int64
-    scanner.Scan()
-    X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    var Y int64
-    scanner.Scan()
-    Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    x := make([]int64, N)
-    y := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        x[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-        scanner.Scan()
-        y[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	var N int
+	scanner.Scan()
+	N, _ = strconv.Atoi(scanner.Text())
+	var X int
+	scanner.Scan()
+	X, _ = strconv.Atoi(scanner.Text())
+	var Y int
+	scanner.Scan()
+	Y, _ = strconv.Atoi(scanner.Text())
+	x := make([]int, N)
+	y := make([]int, N)
+	for i := 0; i < N; i++ {
+		scanner.Scan()
+		x[i], _ = strconv.Atoi(scanner.Text())
+		scanner.Scan()
+		y[i], _ = strconv.Atoi(scanner.Text())
+	}
 	solve(N, X, Y, x, y)
 }
