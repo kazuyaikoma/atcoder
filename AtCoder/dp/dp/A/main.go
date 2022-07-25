92721bced6733861aae3a9daecacbdd16fb05a06
nixii <nixii0986@gmail.com>
2022-07-25 23:38:47 +0900

    :+1: solved dp A

--- AtCoder/dp/dp/A/main.go
+++ AtCoder/dp/dp/A/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
-func solve(N int64, h []int64) {
-
+func solve(N int, h []int) {
+	dp := []float64{0, math.Abs(float64(h[0] - h[1]))}
+	for i := 2; i < N; i++ {
+		diff := math.Min(
+			math.Abs(float64(h[i-2]-h[i]))+dp[i-2],
+			math.Abs(float64(h[i-1]-h[i]))+dp[i-1],
+		)
+		dp = append(dp, diff)
+	}
+	fmt.Println(int(dp[N-1]))
 }
 
 func main() {
@@ -16,13 +26,13 @@ func main() {
 	const maxBufSize = 1000000
 	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
 	scanner.Split(bufio.ScanWords)
-	var N int64
-    scanner.Scan()
-    N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    h := make([]int64, N)
-    for i := int64(0); i < N; i++ {
-        scanner.Scan()
-        h[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
-    }
+	var N int
+	scanner.Scan()
+	N, _ = strconv.Atoi(scanner.Text())
+	h := make([]int, N)
+	for i := 0; i < N; i++ {
+		scanner.Scan()
+		h[i], _ = strconv.Atoi(scanner.Text())
+	}
 	solve(N, h)
 }
